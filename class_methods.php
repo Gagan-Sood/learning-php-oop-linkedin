0e6f97132392c39a77ec7a1cde93bb920b1e05ee
Gagan Sood <gagansood777@gmail.com>
2022-08-02 23:27:11 +0530

    refer to an instance

--- class_methods.php
+++ class_methods.php
@@ -8,6 +8,10 @@ class Student {
     function say_hello() {
         return "Hello World!";
     }
+
+    function full_name() {
+        return $this->first_name .' '. $this->last_name;
+    }
 }
 $student1 = new Student;
 $student1->first_name = "Gagan";
@@ -17,14 +21,14 @@ $student2 = new Student;
 $student2->first_name = "Robin";
 $student2->last_name = "Das";
 
-echo $student1->first_name .' '. $student1->last_name .'<br \>';
-echo $student2->first_name .' '. $student2->last_name .'<br \>';
+echo $student1->full_name() .'<br \>';
+echo $student2->full_name() .'<br \>';
 
 echo $student1->say_hello() .'<br \>';
 echo $student2->say_hello() .'<br \>';
 
 $class_methods = get_class_methods('Student');
-echo 'Class methods:'. implode(', ', $class_methods);
+echo 'Class methods:'. implode(', ', $class_methods) .'<br \>';
 
 if (method_exists('Student','say_hello')) {
     echo "Method say_hello exists inside a class";
