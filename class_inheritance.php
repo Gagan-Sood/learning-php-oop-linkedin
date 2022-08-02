00f13605b119f37f5ce921913a29e26cdb8a0b5b
Gagan Sood <gagansood777@gmail.com>
2022-08-03 01:11:05 +0530

    extend and override a class

--- class_inheritance.php
+++ class_inheritance.php
@@ -1,6 +1,7 @@
 <?php
 
 class User {
+    var $is_admin = false;
     var $first_name;
     var $last_name;
     var $username;
@@ -11,7 +12,21 @@ class User {
 }
 
 class Customer extends User {
+    var $city;
+    var $state;
+    var $country;
 
+    function location() {
+        return $this->city . ", " . $this->state . ", ". $this->country;
+    }
+}
+
+class AdminUser extends User {
+    var $is_admin = true;
+
+    function full_name() {
+        return $this->first_name .' '. $this->last_name . "(Admin)";
+    }
 }
 
 $user = new User;
@@ -23,9 +38,14 @@ $c = new Customer;
 $c->first_name = "George";
 $c->last_name = "Costanza";
 $c->username = "GCostanza";
+$c->city = "New York";
+$c->state = "New York";
+$c->country = "United States";
 
 echo $user->full_name() .'<br \>';
 echo $c->full_name() .'<br \>';
+echo $c->location() .'<br \>';
+// echo $user->location() .'<br \>'; No method error
 
 echo get_parent_class($user) .'<br \>';
 echo get_parent_class($c) .'<br \>';
