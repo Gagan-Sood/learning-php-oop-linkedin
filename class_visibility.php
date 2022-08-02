4536807032ab1f025cb2132e11f2900a7e5c80a1
Gagan Sood <gagansood777@gmail.com>
2022-08-03 02:14:02 +0530

    access modifiers

---
+++ class_visibility.php
@@ -0,0 +1,46 @@
+<?php
+
+use Student as GlobalStudent;
+
+class Student {
+    public $first_name;
+    public $last_name;
+    public $country = 'None';
+
+    protected $registrationId;
+    private $tution = 0.0;
+
+    public function hello_world() {
+        return "Hello World!";
+    }
+
+    protected function hello_family() {
+        return "Hello Family!";
+    }
+
+    private function hello_me() {
+        return "Hello Me!";
+    }
+
+    public function full_name() {
+        return $this->first_name .' '. $this->last_name;
+    }
+}
+
+class PartTimeStudent extends Student {
+    public function hello_parent() {
+        return $this->hello_family();
+    }
+}
+
+$student1 = new PartTimeStudent;
+$student1->first_name = "Gagan";
+$student1->last_name = "Sood";
+
+echo $student1->full_name() .'<br \>';
+echo $student1->hello_world() .'<br \>';
+echo $student1->hello_parent() .'<br \>';
+// echo $student1->hello_Me() .'<br \>';
+// echo $student1->registrationId;
+// echo $student1->tution;
+?>
